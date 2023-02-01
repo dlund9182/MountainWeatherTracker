af0afc1500ab649176811e80c58dc1156b8f9df0
dlund9182 <86538284+dlund9182@users.noreply.github.com>
2023-01-31 16:47:13 -0800

    Update weatherModel.py

--- weatherModel.py
+++ weatherModel.py
@@ -18,47 +18,19 @@ class weatherModel:
         self._attr = attr
         
     def _setTag(self,tag):
-        """
-            
-        Input
-          tag to set the protected variable tag in class weatherModel
-      
-        Returns nothing(void)  
-    
-        """         
-
         self._tag = tag
         
     def _setAttr(self,attr):
-        """
-        
-        Input
-          attr to set the protected variable attr in class weatherModel
-          
-        Returns nothing(void)
-        
-        """   
         self._attr = attr         
             
     def _getArray(self,start,end,soup,option = ""):
-        """
         
-        Input
-          start: the startTH point in the array local array results
-          end: the endTH point in the local array results
-          soup: The beautiful soup object
-          option: optional argument that defaults to "". 
-          
-        Returns nothing(void)
-        
-        return an arrray of end-start + 1
-        
-        """
         results = soup.find_all(self._tag,self._attr)
         arr = []
-        count = 0
-
+        count = 0 
+        
         for result in results:
+            
             if count < start:
                 count = count + 1
                 continue
@@ -72,66 +44,27 @@ class weatherModel:
         return(arr)
     
     def __getResults(self,outer,option):
-        """
-        
-       I will get around to documenting this later since its bit esoteric
-       and not strait forward to explain
-        
-        """
         if (option == "temp"):
-            inner = outer.find('span',{'class':'temperature__label nrk-sr'})
-            if (inner):
-                return(outer.text.split('Temperature')[1][:-1])      
-            else:
-                return(0.0)
-        
+            return (outer.get_text()[:-1])
+
         return(outer.string)
         
-    def _convertToNum(self,number_string):
-        """
-        
-        Input
-          number_string which is a string which in most cases will be digits
-          unless it just has "-"
-          
-        Returns a float represenation of number_string unless it is just "-"
-        in which case it returns 0
-        
-        """
-        if (number_string == "-"):
-            return(0.0)
+    def _convertToNum(self,n):
+        if (n == "-") or (n == None):
+            return(0)
         else:
-            return(float(number_string))
+            return(float(n))
     
         
-    def _convertAll(self,arr):
-        """
-        
-        Input
-           arr which is an array of strings consisting either of just digits
-           or just a "-"
-          
-        Returns a list(array) of numbers(floats)
-          
-        
-        """
-        return(list(map(self._convertToNum, arr)))
+    def _convertAll(self,Arr):
+        return(list(map(self._convertToNum, Arr)))
     
-    def _avg(self,arr):
-        """
-        
-        Input
-           arr which is an array of floats
-          
-        Returns the average of the array arr
-          
-        
-        """
-    
-        Arr = self._convertAll(arr)
-        return(sum(arr) / len(arr))
+    def _avg(self,Arr):
+        Arr = self._convertAll(Arr)
+        return(sum(Arr) / len(Arr))
     
     def setSoap(self):
         self._soup = BeautifulSoup(requests.get(self._file_url).text,"lxml")
 
+    
     
